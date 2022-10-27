2a40b0d468ca18cc7af61d96c71a92b57c5e2a28
AlanConstantine <rlalan@outlook.com>
2022-10-28 07:54:01 +0800

    update

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,1 +1,4 @@
+HKU_DIR = r'./HKU956/1. physiological_signals/'
+KEC_DIR = r''
+
 sample_rate = {'BVP': 64, 'EDA': 4, 'HR': 1, 'TEMP': 4}
--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
+from scipy.signal import freqs
 from scipy.signal import butter, lfilter
 import numpy as np
+import os
 
 
 def butter_bandpass(lowcut, highcut, fs, order=4):
@@ -10,3 +12,20 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, data)
     return y
+
+
+def butter_lowpass(cutOff, fs, order=5):
+    nyq = 0.5 * fs
+    normalCutoff = cutOff / nyq
+    b, a = butter(order, normalCutoff, btype='low', analog=True)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutOff, fs, order=4):
+    b, a = butter_lowpass(cutOff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+def get_info(filename):
+    filename, extension = os.path.split(filename)
