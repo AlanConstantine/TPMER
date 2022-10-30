5bc0a6c5fc57fe4675a8eef7c4853498babb249a
AlanConstantine <rlalan@outlook.com>
2022-10-30 14:42:48 +0800

    update

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,6 +1,8 @@
 HKU_DIR = r'./HKU956/1. physiological_signals/'  # HKU956 dir
 KEC_DIR = r''  # KEmoCon dir
 
+PROCESSED_DIR = r'./processed_signal'
+
 SIGNALS = ['BVP', 'EDA', 'HR', 'TEMP', 'IBI']
 SAMPLERATE = {'BVP': 64, 'EDA': 4, 'HR': 1, 'TEMP': 4}
 CUTOFF = {'BVP': [1, 8], 'TEMP': [0.005, 0.1], 'EDA': 2}
