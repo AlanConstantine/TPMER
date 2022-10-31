e72ae668371242f7314d10b0f4d2db3998bdb699
AlanConstantine <rlalan@outlook.com>
2022-11-01 01:09:29 +0800

    update

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import pickle
 
 
 def resample_by_poly(signal, input_fs, output_fs):
@@ -97,3 +98,55 @@ def get_folder_files(folderpath):
         for file in files:
             folderfiles.append(os.path.join(root, file))
     return folderfiles
+
+
+def reduce_mem_usage(df, un_process, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    orig_cols = len(df.columns.values)
+    for col in df.columns:
+        if col in un_process:
+            continue
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose:
+        print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(
+            end_mem, 100 * (start_mem - end_mem) / start_mem))
+    print('Final', df.shape)
+    return df
+
+
+def writetxt(filaname, line):
+    with open(filaname, 'a') as f:
+        f.write(line)
+
+
+def save_model(filename, model):
+    with open(filename, 'wb') as f:
+        pickle.dump(model, f)
+    print(filename, 'saved done!')
+
+
+def load_model(filename):
+    with open(filename, 'rb') as f:
+        model = pickle.load(f)
+    return model
