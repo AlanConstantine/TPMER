c058e19d7108c0b2896a8d2d4815835b6d7ea2e5
AlanConstantine <rlalan@outlook.com>
2023-01-25 10:45:46 +0800

    update

--- representation/run.py
+++ representation/run.py
@@ -9,6 +9,7 @@ from torch.utils.data import (
     TensorDataset, DataLoader, SequentialSampler, WeightedRandomSampler)
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
+from sklearn.model_selection import train_test_split
 import torch.optim as optim
 import torch.nn.functional as F
 from torch import nn
@@ -36,15 +37,35 @@ def init_xavier(m):
 
 
 class DataPrepare(object):
-    def __init__(self, datapath=r'../processed_signal/all_400_4s_step_2s.pkl') -> None:
-        self.df = pd.read_pickle(datapath)
-        # del self.df['participant_id'], self.df['source']
+    def __init__(self, args, datapath=r'../processed_signal/all_400_4s_step_2s.pkl') -> None:
+        self.args = args
+        self.data = pd.read_pickle(datapath)
+        self.drop_columns()
+        self.randomization()
+        print('Data size:', self.data.shape)
+
+        X_train, X_test = train_test_split(
+            self.data, test_size=0.2, random_state=3131, shuffle=True)
+
+        if self.args.debug:
+            X_train, X_test = X_train[:1000], X_test[:100]
+
+        X_train = torch.from_numpy(X_train).to(torch.float32)
+        X_test = torch.from_numpy(X_test).to(torch.float32)
+
+        self.X_train, self.X_test = X_train.to(
+            self.args.device), X_test.to(self.args.device)
+
+        self.batch_size = self.args.batch_size
+
+        print('Splited size:', X_train.shape, X_test.shape)
 
     def randomization(self):
-        self.df = self.df.sample(frac=1)
+        self.data = self.data.sample(frac=1)
 
     def drop_columns(self):
-        pass
+        self.data = self.data.loc[:, ~self.data.columns.isin(
+            ['participant_id', 'source'])]
 
 
 def train():
@@ -60,8 +81,7 @@ def run():
 
 
 def main():
-
-    print(data.shape)
+    dataprepare = DataPrepare()
 
 
 if __name__ == '__main__':
