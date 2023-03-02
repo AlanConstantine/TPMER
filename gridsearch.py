765c609341b9671ec68943cc2e05c76a4f2032b9
AlanConstantine <rlalan@outlook.com>
2023-03-02 14:53:45 +0800

    update

--- gridsearch.py
+++ gridsearch.py
@@ -4,16 +4,16 @@ from tools import *
 import os
 
 data_dict = [
-    {'data': r'./processed_signal/HKU956/1540_24s_step_2s.pkl',
-        'spliter': r'./processed_signal/HKU956/1540_24s_step_2s_spliter5.pkl'},
+    # {'data': r'./processed_signal/HKU956/1540_24s_step_2s.pkl',
+    #     'spliter': r'./processed_signal/HKU956/1540_24s_step_2s_spliter5.pkl'},
     {'data': r'./processed_signal/HKU956/772_12s_step_2s.pkl',
         'spliter': r'./processed_signal/HKU956/772_12s_step_2s_spliter5.pkl'},
     {'data': r'./processed_signal/HKU956/516_8s_step_2s.pkl',
         'spliter': r'./processed_signal/HKU956/516_8s_step_2s_spliter5.pkl'},
     {'data': r'./processed_signal/HKU956/260_4s_step_2s.pkl',
         'spliter': r'./processed_signal/HKU956/260_4s_step_2s_spliter5.pkl'},
-    {'data': r'./processed_signal/HKU956/772_12s_step_6s.pkl',
-        'spliter': r'./processed_signal/HKU956/772_12s_step_6s_spliter5.pkl'},
+    # {'data': r'./processed_signal/HKU956/772_12s_step_6s.pkl',
+    #     'spliter': r'./processed_signal/HKU956/772_12s_step_6s_spliter5.pkl'},
 ]
 
 targets = ['valence_label', 'arousal_label']
@@ -24,7 +24,8 @@ for e in data_dict:
     data = e['data']
     spliter = e['spliter']
     for targ in targets:
-        args = Params(target=targ, debug=False, data=data, spliter=spliter, use_cuda=True, batch_size=128)
+        args = Params(target=targ, debug=False, data=data,
+                      spliter=spliter, use_cuda=True, batch_size=128)
         results = api(args=args)
         all_results.append([args.save_path, results])
         print('\n\n\n')
